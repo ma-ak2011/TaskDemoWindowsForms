aa6d8c2cd602dd225376a2136be4358090b60bcb
ma-ak2011 <ponyonekoak@gmail.com>
2018-05-30 04:33:08 +0900

    初回コミット

---
+++ TaskDemoWindowsForms/Form1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TaskDemoWindowsForms
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "処理中。。。。";
+            textBox1.Refresh();
+            Thread.Sleep(10000);
+            textBox1.Text = "Done!";
+        }
+    }
+}
