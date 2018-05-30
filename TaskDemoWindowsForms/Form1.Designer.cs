f02e52cef69c3e28e24d363fee9e91e04a9eb8a3
ma-ak2011 <ponyonekoak@gmail.com>
2018-05-31 07:03:05 +0900

    完成

--- TaskDemoWindowsForms/Form1.Designer.cs
+++ TaskDemoWindowsForms/Form1.Designer.cs
@@ -30,15 +30,20 @@
         {
             this.button1 = new System.Windows.Forms.Button();
             this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(12, 279);
+            this.button1.Location = new System.Drawing.Point(12, 174);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.Size = new System.Drawing.Size(268, 23);
             this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
+            this.button1.Text = "重い処理を同期で";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
@@ -47,14 +52,69 @@
             this.textBox1.Location = new System.Drawing.Point(12, 12);
             this.textBox1.Multiline = true;
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(355, 261);
+            this.textBox1.Size = new System.Drawing.Size(685, 156);
             this.textBox1.TabIndex = 1;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(12, 216);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(268, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "重い処理を非同期で";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(12, 301);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(268, 23);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "3つの重い処理を非同期で";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(344, 174);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(102, 23);
+            this.button4.TabIndex = 4;
+            this.button4.Text = "フリーズしてる？";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(12, 347);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(268, 23);
+            this.button5.TabIndex = 5;
+            this.button5.Text = "async-await 3つの重い処理を非同期で";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(12, 259);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(268, 23);
+            this.button6.TabIndex = 6;
+            this.button6.Text = "async-await 重い処理を非同期で";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(388, 314);
+            this.ClientSize = new System.Drawing.Size(709, 382);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.button1);
             this.Name = "Form1";
@@ -68,6 +128,11 @@
 
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
     }
 }
 
--- TaskDemoWindowsForms/Form1.cs
+++ TaskDemoWindowsForms/Form1.cs
@@ -22,8 +22,112 @@ namespace TaskDemoWindowsForms
         {
             textBox1.Text = "処理中。。。。";
             textBox1.Refresh();
-            Thread.Sleep(10000);
-            textBox1.Text = "Done!";
+            Thread.Sleep(5000);
+            textBox1.Text += "\r\n終了!";
         }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "処理中。。。。";
+
+            Task.Factory.StartNew(() => Thread.Sleep(5000))
+                .ContinueWith(t =>
+                {
+                    textBox1.Text += "\r\n終了!";
+                }, 
+                TaskScheduler.FromCurrentSynchronizationContext());
+            
+        }
+
+        private async void button6_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "処理中。。。。";
+
+            await Task.Factory.StartNew(() => Thread.Sleep(5000));
+
+            textBox1.Text += "\r\n終了!";
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "処理中。。。。(スレッドID：" + Thread.CurrentThread.ManagedThreadId + ")";
+
+            Task.Factory
+                .StartNew(() =>
+                {
+                    Thread.Sleep(3000);
+                    return "非同期の結果1 (スレッドID：" + Thread.CurrentThread.ManagedThreadId + ")";
+                })
+                .ContinueWith(t1 =>
+                {
+                    textBox1.Text += "\r\n" + t1.Result;
+
+                    Task.Factory.StartNew(() =>
+                    {
+                        Thread.Sleep(3000);
+                        return t1.Result + " + 非同期の結果2 (スレッドID：" + Thread.CurrentThread.ManagedThreadId +")";
+
+                    }, CancellationToken.None ,TaskCreationOptions.None, TaskScheduler.Default)
+                    .ContinueWith(t2 =>
+                    {
+                        textBox1.Text += "\r\n" + t2.Result;
+                        
+                        Task.Factory.StartNew(() =>
+                        {
+                            Thread.Sleep(3000);
+                            return t2.Result + " + 非同期の結果3 (スレッドID：" + Thread.CurrentThread.ManagedThreadId + ")";
+
+                        }, CancellationToken.None, TaskCreationOptions.None, TaskScheduler.Default)
+                        .ContinueWith(t3 =>
+                        {
+                            textBox1.Text += "\r\n" + t3.Result;
+                            textBox1.Text += "\r\n終了！(スレッドID：" + Thread.CurrentThread.ManagedThreadId + ")";
+                        }, 
+                        TaskScheduler.FromCurrentSynchronizationContext());
+                    },
+                    TaskScheduler.FromCurrentSynchronizationContext());
+                },
+                TaskScheduler.FromCurrentSynchronizationContext());
+        }
+
+        private async void button5_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "処理中。。。。(スレッドID：" + Thread.CurrentThread.ManagedThreadId + ")";
+
+            var result1 = await Task.Factory.StartNew(() =>
+            {
+                Thread.Sleep(3000);
+                return "非同期の結果1 (スレッドID：" + Thread.CurrentThread.ManagedThreadId + ")";
+            });
+
+            textBox1.Text += "\r\n" + result1;
+
+            var result2 = await Task.Factory.StartNew(() =>
+            {
+                Thread.Sleep(3000);
+                return result1 + " + 非同期の結果2 (スレッドID：" + Thread.CurrentThread.ManagedThreadId + ")";
+
+            });
+
+            textBox1.Text += "\r\n" + result2;
+
+            var result3 = await Task.Factory.StartNew(() =>
+            {
+                Thread.Sleep(3000);
+                return result2 + " + 非同期の結果3 (スレッドID：" + Thread.CurrentThread.ManagedThreadId + ")";
+
+            });
+
+            textBox1.Text += "\r\n" + result3;
+            textBox1.Text += "\r\n終了！(スレッドID：" + Thread.CurrentThread.ManagedThreadId + ")";
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            var random = new Random();
+            button4.BackColor = Color.FromArgb(random.Next(256), random.Next(256), random.Next(256));
+        }
+
+        
     }
 }
